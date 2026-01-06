0b4fb8c013cc2e13f547e5dbe75482ac66e06e24
kathiravan <kathiravan.sse@saveetha.com>
2026-01-06 15:40:40 +0530

    Initial Commit

---
+++ VSN Home/VSN_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VSN_HomeApp.swift
+//  VSN Home
+//
+//  Created by SAIL on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct VSN_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
